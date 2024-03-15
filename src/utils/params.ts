6b3e937e7fd725acbd453a80a78975d52d1d834d
Hasbi <hasbi@ret.co.id>
2024-03-15 14:02:14 +0700

    Add new validation schemas and interfaces for user and material

--- src/utils/params.ts
+++ src/utils/params.ts
@@ -1,8 +1,14 @@
 import { InferType } from 'yup'
-import { LoginSchema, RegisterSchema } from './validations/user/user-validation'
+import { LoginSchema, RegisterSchema, UpdateUserSchema, UserCitizenSchema } from './validations/user/user-validation'
+import { Material } from '@prisma/client'
 
 export interface UserCreateParam extends InferType<typeof RegisterSchema> {}
+export interface UserUpdateParam extends InferType<typeof UpdateUserSchema> {}
+export interface UserAddCitizenParam extends InferType<typeof UserCitizenSchema> {}
 export interface UserLoginParam extends InferType<typeof LoginSchema> {}
+
+//* Material Related
+export interface MaterialParam extends Partial<Material> {}
 
 export interface UserParam {
   id: string
