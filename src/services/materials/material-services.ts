687d72c382e82eb7f72350970da6ac49ef0cab38
Hasbi <hasbi@ret.co.id>
2024-03-15 14:01:44 +0700

    Add MaterialServices class with create, getAll, findById, update, and delete methods

---
+++ src/services/materials/material-services.ts
@@ -0,0 +1,167 @@
+import { PrismaErrorHandle, prismaClient } from '../../config/database'
+import { ResponseError } from '../../utils/error-response'
+import { MaterialParam } from '../../utils/params'
+import { MaterialCreateSchema } from '../../utils/validations/material/material-validation'
+import { Validate } from '../../utils/validations/validate'
+
+export class MaterialServices {
+  /***
+   * Material Service
+   * @desc Create Material
+   * @param {MaterialParam} param
+   * @return {Promise<any>}
+   * @throws {ResponseError}
+   * */
+  async create(param: MaterialParam): Promise<any> {
+    const data: MaterialParam = await Validate(MaterialCreateSchema, param)
+
+    const alreadyExist = await prismaClient.material.count({
+      where: {
+        name: data.name
+      }
+    })
+
+    console.log('alreadyExist', alreadyExist)
+
+    if (alreadyExist > 0) {
+      throw new ResponseError(400, 'Material already exist')
+    }
+
+    const material = await prismaClient.material
+      .create({
+        data: {
+          name: data.name ?? '',
+          base_price: data.base_price,
+          unit: data.unit ?? ''
+        }
+      })
+      .catch((error) => {
+        PrismaErrorHandle(error)
+      })
+
+    return material
+  }
+
+  /***
+   * Material Service
+   * @desc Get All Material
+   * @return {Promise<any>}
+   * */
+  async getAll(): Promise<any> {
+    const data = await prismaClient.material.findMany({
+      where: {
+        is_deleted: false,
+        is_active: true,
+        deleted_at: null
+      }
+    })
+    return data
+  }
+
+  /***
+   * Material Service
+   * @desc Get Material By Id
+   * @param {string} id
+   * @return {Promise<any>}
+   * */
+
+  async findById(id: string): Promise<any> {
+    if (!id || id === '' || id === undefined || id === null) {
+      throw new ResponseError(400, 'Material Id is required')
+    }
+
+    console.log('findById:material', id)
+    const data = await prismaClient.material.findUnique({
+      where: {
+        id: id,
+        is_deleted: false
+      }
+    })
+
+    if (!data) {
+      throw new ResponseError(400, 'Material not found')
+    }
+    return data
+  }
+
+  /***
+   * Material Service
+   * @desc Update Material
+   * @param {MaterialParam} param
+   * @return {Promise<any>}
+   * @throws {ResponseError}
+   * */
+  async update(id: any, param: MaterialParam): Promise<any> {
+    if (!id || id === '' || id === undefined || id === null) {
+      throw new ResponseError(400, 'Material Id is required')
+    }
+    const data: MaterialParam = await Validate(MaterialCreateSchema, param)
+    const material = await prismaClient.material
+      .update({
+        where: {
+          id: id
+        },
+        data: {
+          name: data.name ?? '',
+          base_price: data.base_price,
+          unit: data.unit ?? ''
+        }
+      })
+      .catch((error) => {
+        PrismaErrorHandle(error)
+      })
+
+    return material
+  }
+
+  /***
+   * Material Service
+   * @desc Delete Material
+   * @param {string} id
+   * @return {Promise<any>}
+   * @throws {ResponseError}
+   *
+   * */
+  async delete(id: any): Promise<any> {
+    if (!id || id === '' || id === undefined || id === null) {
+      throw new ResponseError(400, 'Material Id is required')
+    }
+
+    const isExist = await prismaClient.material.count({
+      where: {
+        id: id
+      }
+    })
+
+    if (isExist < 1) {
+      throw new ResponseError(400, 'Material not found')
+    }
+
+    const alreadyDeleted = await prismaClient.material.count({
+      where: {
+        id: id,
+        is_deleted: true
+      }
+    })
+
+    if (alreadyDeleted > 0) {
+      throw new ResponseError(400, 'Material already deleted')
+    }
+    const material = await prismaClient.material
+      .update({
+        where: {
+          id: id
+        },
+        data: {
+          is_deleted: true,
+          deleted_at: new Date(),
+          is_active: false
+        }
+      })
+      .catch((error) => {
+        PrismaErrorHandle(error)
+      })
+
+    return material
+  }
+}
