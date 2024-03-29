dbc5941d4619e1bb152e0be52fa44623849e9383
spaceark7 <spacearkchief@gmail.com>
2024-03-30 02:58:10 +0700

    Add DateParamParser function to helper.ts

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,3 +1,18 @@
 export const StringConcate = (...strings: string[]) => {
   return strings.join(' ')
 }
+
+export const DateParamParser = (date: string, arg_date: string) => {
+  if (!date) {
+    return undefined
+  }
+  if (arg_date === 'gte') {
+    return {
+      gte: new Date(date)
+    }
+  } else {
+    return {
+      lte: new Date(date)
+    }
+  }
+}
