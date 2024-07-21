93fd648cf03187c061329b840647a058021d7e91
spaceark7 <spacearkchief@gmail.com>
2024-07-21 20:28:11 +0700

    experimental: query string parser

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,3 +1,5 @@
+import queryString from 'query-string'
+import { FilterParam } from './params'
 export const StringConcate = (...strings: string[]) => {
   return strings.join(' ')
 }
@@ -16,4 +18,20 @@ export const DateParamParser = (date: string, arg_date: string) => {
       lte: new Date(date)
     }
   }
+}
+
+export const ParseQueryFilter = (filter: FilterParam extends Record<any, any> ? FilterParam : any) => {
+  // const a = await queryString
+  // if (a) {
+  //   return a.parse(filter as any)
+  // }
+  Object.keys(filter).forEach((key) => {
+    if (filter[key] == null || filter[key] === '') {
+      delete filter[key]
+    } else if (filter[key].includes('{')) {
+      filter[key] = JSON.parse(filter[key])
+    }
+  })
+
+  return filter
 }
